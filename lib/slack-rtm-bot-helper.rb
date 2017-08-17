d8a630964983cb08fa0879f6de79ae9dbbab515a
littlekbt <kr.kubota.11@gmail.com>
2017-08-17 15:25:40 +0900

    fix require library

--- lib/slack-rtm-bot-helper.rb
+++ lib/slack-rtm-bot-helper.rb
@@ -3,7 +3,6 @@ require 'open-uri'
 require 'faraday'
 require 'json'
 require 'uri'
-require 'yaml'
 
 require 'slack-rtm-bot-helper/version'
 require 'slack-rtm-bot-helper/helper'
--- lib/slack-rtm-bot-helper/version.rb
+++ lib/slack-rtm-bot-helper/version.rb
@@ -2,7 +2,7 @@ module Slack
   module Rtm
     module Bot
       class Helper
-        VERSION = "0.1.0"
+        VERSION = "0.1.1"
       end
     end
   end
